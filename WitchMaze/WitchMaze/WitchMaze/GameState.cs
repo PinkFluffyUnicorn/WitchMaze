4a83f35f645b192a21d7fb7d492dc49425a3c57f
tobeneck <Tobias.benecke@st.ocgu.de>
2015-05-16 11:29:18 +0200

    Created Projekt //u don´t say :O

---
+++ WitchMaze/WitchMaze/WitchMaze/GameState.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WitchMaze
+{
+    public interface GameState
+    {
+        void initialize();
+
+        void loadContent();
+
+        void unloadContent();
+
+        EGameState update(GameTime gameTime);
+
+        void Draw(GameTime gameTime);
+    }
+
+    public enum EGameState
+    {
+        MainMenu,
+        InGame,
+        Credits,
+        Options,
+        Exit,
+    }
+}
---
+++ WitchMaze/WitchMaze/WitchMaze/GameStates/Credits.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WitchMaze.GameStates
+{
+    class Credits : GameState
+    {
+        public Credits() { }
+
+        public void initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void loadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void unloadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EGameState update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
