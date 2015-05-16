c88015e0296819152525ca9a763e8559f57f09b8
toschwar <schwarzbach.tom@web.de>
2015-05-16 15:32:56 +0200

    MainMenu.cs: -IsPressed-Contents              -draw (incomplete)              -loadContent (errors)

--- WitchMaze/WitchMaze/WitchMaze/GameStates/MainMenu.cs
+++ WitchMaze/WitchMaze/WitchMaze/GameStates/MainMenu.cs
@@ -4,25 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+
 
 namespace WitchMaze.GameStates
 {
     class MainMenu : GameState
-    {
+    {   
+        
+        GraphicsDeviceManager graphics;
+        
         int count = 0;
         bool isPressed = false;
 
         KeyboardState keyboard = Keyboard.GetState();
 
+        Texture2D startGameIsNotPressed;
+        SpriteBatch StartGameIsNotPressed;
+        
+        Texture2D startGameIsPressed;
+        SpriteBatch StartGameIsPressed;
+        
         Texture2D optionsIsNotPressed;
         SpriteBatch OptionsIsNotPressed;
-       
         
+        Texture2D optionsIsPressed;
+        SpriteBatch OptionsIsPressed;
+        
+        Texture2D creditsIsNotPressed;
+        SpriteBatch CreditsIsNotPressed;
+        
+        Texture2D creditsIsPressed;
+        SpriteBatch CreditsIsPressed;
+        
+        Texture2D exitIsNotPressed;
+        SpriteBatch ExitIsNotPressed;
+        
+        Texture2D exitIsPressed;
+        SpriteBatch ExitIsPressed;
+          
+      
         public void initialize()
         {
     
@@ -31,8 +57,31 @@ namespace WitchMaze.GameStates
 
         public void loadContent()
         {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            OptionsIsNotPressed = new SpriteBatch(GraphicsDevice);
             optionsIsNotPressed = Content.Load<Texture2D>("options");
+
+            OptionsIsPressed = new SpriteBatch(GraphicsDevice);
+            optionsIsPressed = Content.Load<Texture2D>("optionsIsPressed");
+
+            ExitIsNotPressed = new SpriteBatch(GraphicsDevice);
+            exitIsNotPressed = Content.Load<Texture2D>("exit");
+
+            ExitIsPressed = new SpriteBatch(GraphicsDevice);
+            exitIsPressed = Content.Load<Texture2D>("exitIsPressed");
+
+            CreditsIsNotPressed = new SpriteBatch(GraphicsDevice);
+            creditsIsNotPressed = Microsoft.Xna.Framework.Content.Load<Texture2D>("credits");
+
+            CreditsIsPressed = new SpriteBatch(GraphicsDevice);
+            creditsIsPressed = Content.Load<Texture2D>("creditsIsPressed");
+
+            StartGameIsNotPressed = new SpriteBatch(GraphicsDevice);
+            startGameIsNotPressed = Content.Load<Texture2D>("startGame");
+
+            StartGameIsPressed = new SpriteBatch(GraphicsDevice);
+            startGameIsPressed = Content.Load<Texture2D>("startGameIsPressed");
+            
+          
             
         }
 
@@ -67,31 +116,10 @@ namespace WitchMaze.GameStates
 
         public void Draw(Microsoft.Xna.Framework.GameTime gameTime, Microsoft.Xna.Framework.GraphicsDeviceManager graphics)
         {
-
-           /* graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // Draw the sprite.
-            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
-            spriteBatch.Draw(myTexture, spritePosition, Color.White);
-            spriteBatch.End();
-
-            base.Draw(gameTime);
-
-            //hier werden die Fenster gezeichnet
-            if (count % 3 == 0)
-                window.Draw(StartIsPressed);
-            else
-                window.Draw(Start);
-            if (count % 3 == 1)
-                window.Draw(CreditsIsPressed);
-            else
-                window.Draw(Credits);
-            if (count % 3 == 2)
-                window.Draw(ExitIsPressed);
-            else
-                window.Draw(Exit);*/
+            graphics.GraphicsDevice.Clear(Color.Black);
 
 
+            
             if (!keyboard.IsKeyDown(Keys.W) && !keyboard.IsKeyDown(Keys.S) && !keyboard.IsKeyDown(Keys.Up) && !keyboard.IsKeyDown(Keys.Down))
                 isPressed = false;
         }
