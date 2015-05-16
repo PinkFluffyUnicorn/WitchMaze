2312fd98b9b74573e66d04df67c35d8109cb0b94
Sara <sara.zecha@unitybox.de>
2015-05-16 15:48:31 +0200

    andere Map

--- WitchMaze/WitchMaze/WitchMaze/Map/MapCreator.cs
+++ WitchMaze/WitchMaze/WitchMaze/Map/MapCreator.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using WitchMaze.Map.Blocks;
 
 
 namespace WitchMaze.Map
@@ -15,6 +16,7 @@ namespace WitchMaze.Map
     class MapCreator
     {
         public int[,] mapType;
+        public Map map;
         // Types(weil ich nicht weiß, ob hier ein enum shcon sinn macht ^^
         // 0: Floor, 1: Wall, 2: Blackhole
 
@@ -23,16 +25,24 @@ namespace WitchMaze.Map
             mapType = new int[Settings.mapSizeX, Settings.mapSizeZ];
         }
 
-        public void initialize()
+
+        public Map generateMap()
         {
-            //some funny Random stuff which fills the array with useful numbers 
             for (int i = 0; i < Settings.mapSizeX; i++)
             {
                 for (int j = 0; i < Settings.mapSizeZ; j++)
                 {
                     mapType[i, j] = 0;
+                    if (mapType[i, j] == 0)
+                        map.map[i, j] = new Floor(new Vector3((float)(i * Settings.blockSizeX), 0f, (float)(j * Settings.blockSizeZ)), Settings.floorColor);
+                    if (mapType[i, j] == 1)
+                        map.map[i, j] = new Wall();
+                    else
+                        map.map[i, j] = new BlackHole();
+
                 }
             }
+            return map;
         }
 
 
