df48b0a3189cdac7a3b66427871a38c2680a57f0
toschwar <schwarzbach.tom@web.de>
2015-08-20 20:31:11 +0200

    skyboxModel und glattes WallModel außerdem noch texturen für options (also buttons und so) und eine angefangene optionsKlasse

--- WitchMaze/WitchMaze/WitchMaze/GameStates/Options.cs
+++ WitchMaze/WitchMaze/WitchMaze/GameStates/Options.cs
@@ -9,6 +9,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using WitchMaze.InterfaceObjects;
 
 
 namespace WitchMaze.GameStates
@@ -16,6 +17,16 @@ namespace WitchMaze.GameStates
     class Options : GameState 
     {
         KeyboardState keyboard = Keyboard.GetState();
+        int count = 0;
+        bool isPressed = false;
+
+        float distY = 96;//die abstände zwischen den Texturen in y-richtung ist 96 bei 1080p, ergibt sich aus button höhe und so...
+        float offset = 10;//offset zwischen Icons und Switches
+
+        Vector2 resolutionIconPosition = new Vector2(50, 50);
+
+        Icon resolutionIcon, fullscreenIcon;
+        LeftRightSwitch resolutionLR, fullscreenLR;
         
         public void initialize()
         {
@@ -23,8 +34,22 @@ namespace WitchMaze.GameStates
         }
 
         public void loadContent() 
-        { 
+        {
+            if (Game1.getGraphics() != null)
+            {
+                distY *= Settings.getInterfaceScale();
+                offset *= Settings.getInterfaceScale();
 
+                resolutionIcon = new Icon(resolutionIconPosition, "Textures/option/Resolution");
+                fullscreenIcon = new Icon(new Vector2(50, (resolutionIcon.getPosition().Y + resolutionIcon.getHeight()) + distY), "Textures/option/Fullscreen");
+
+                String[] resolutions = { "Textures/option/1080p", "Textures/option/720p" };
+                String[] fullscreenmode = { "Textures/option/offButton", "Textures/option/onButton" };
+
+                resolutionLR = new LeftRightSwitch(new Vector2(resolutionIcon.getPosition().X + resolutionIcon.getWidth() + offset, resolutionIcon.getPosition().Y), resolutions);
+                fullscreenLR = new LeftRightSwitch(new Vector2(fullscreenIcon.getPosition().X + fullscreenIcon.getWidth() + offset, fullscreenIcon.getPosition().Y), fullscreenmode);
+                
+            }
         }
 
         public void unloadContent() 
@@ -39,6 +64,14 @@ namespace WitchMaze.GameStates
                 return EGameState.MainMenu;
             else 
                 return EGameState.Options; 
+
+        }
+
+        public void updateOptions()
+        {
+            //resolutionLR.setSelected();
+
+ 
         }
 
         public void Draw(GameTime gameTime) 
@@ -46,6 +79,13 @@ namespace WitchMaze.GameStates
             Game1.getGraphics().GraphicsDevice.BlendState = BlendState.Opaque;
             Game1.getGraphics().GraphicsDevice.DepthStencilState = DepthStencilState.Default;
             Game1.getGraphics().GraphicsDevice.Clear(Color.DarkGreen);
+
+            resolutionIcon.draw();
+            resolutionLR.draw();
+
+            fullscreenIcon.draw();
+            fullscreenLR.draw();
+
 
         }
 
