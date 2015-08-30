edf29de2131ce4d6b46eb9f02c1c56fcb4b69760
Sara <sara.zecha@unitybox.de>
2015-08-30 12:54:51 +0200

    start of Lighting of kristall

--- WitchMaze/WitchMaze/WitchMaze/ItemStuff/Items/Serius/Crystal.cs
+++ WitchMaze/WitchMaze/WitchMaze/ItemStuff/Items/Serius/Crystal.cs
@@ -15,7 +15,7 @@ namespace WitchMaze.ItemStuff.Items
         public Crystal(Vector3 _position)
         {
             ambient = new Vector3(1f, 1f, 1f);
-            emissive = new Vector3(1f, 1f, 1f);
+            emissive = new Vector3(0f, 0f, 0f);
             specularColor = new Vector3(1f, 1f, 1f);
             directionalDiffuse = new Vector3(1f, 1f, 1f);
             directionalDirection = new Vector3(1f, 1f, 1f);
